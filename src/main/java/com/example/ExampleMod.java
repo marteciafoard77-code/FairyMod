f1bd2e3a42a6eb08b724bab87a147b02adf321c9
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:24:57 -0500

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,26 +1,19 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+import com.example.item.ModItems; // <- import your ModItems class
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "modid";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// Define the Fairy Charm item
-	public static final Item FAIRY_CHARM = new FairyCharm();
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
 
-	@Override
-	public void onInitialize() {
-		// Register the Fairy Charm item
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fairy_charm"), FAIRY_CHARM);
-
-		LOGGER.info("Hello Fabric world! Fairy Charm has been registered.");
-	}
+        // Register your Fairy Charm item
+        ModItems.registerItems();
+    }
 }
