2231824f5be9f117c396495e6a83d7464a1b40d8
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 15:20:17 -0500

    Update ExampleModClient.java

--- src/main/java/com/example/ExampleModClient.java
+++ src/main/java/com/example/ExampleModClient.java
@@ -1,1 +1,16 @@
 
+package com.example;
+
+import com.example.entity.FairyEntity;
+import com.example.entity.client.FairyRenderer;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Register Fairy renderer so Minecraft knows how to draw it
+        EntityRendererRegistry.register(FairyEntity.class, (context) -> new FairyRenderer(context));
+    }
+}
