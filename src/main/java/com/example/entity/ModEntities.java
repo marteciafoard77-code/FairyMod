69d6318087c17d70d8fd4ce0951c058b46ac5c0a
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:21:30 -0500

    Update ModEntities.java

--- src/main/java/com/example/entity/ModEntities.java
+++ src/main/java/com/example/entity/ModEntities.java
@@ -1,1 +1,22 @@
+package com.example.entity;
+
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import com.example.ExampleMod;
+
+public class ModEntities {
+
+    public static final EntityType<FairyEntity> FAIRY = Registry.register(
+            Registry.ENTITY_TYPE,
+            new Identifier(ExampleMod.MOD_ID, "fairy"),
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FairyEntity::new)
+                    .dimensions(EntityDimensions.fixed(0.5f, 0.9f)) // small size
+                    .build()
+    );
+}
 
