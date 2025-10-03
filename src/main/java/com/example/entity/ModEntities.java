ab81a30e1d1f31fa01a84e681ffacc766d183fae
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:43:16 -0500

    Update ModEntities.java

--- src/main/java/com/example/entity/ModEntities.java
+++ src/main/java/com/example/entity/ModEntities.java
@@ -1,28 +1,36 @@
 package com.example.entity;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.Heightmap;
-import net.minecraft.world.biome.BiomeKeys;
-import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.world.biome.Biome;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 
 import com.example.ExampleMod;
 
 public class ModEntities {
 
-    // Register Fairy entity type
     public static final EntityType<FairyEntity> FAIRY = Registry.register(
             Registry.ENTITY_TYPE,
             new Identifier(ExampleMod.MOD_ID, "fairy"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, FairyEntity::new)
-                    .dimensions(EntityDimensions.fixed(0.5f, 0.9f)) // small size
+                    .dimensions(EntityDimensions.fixed(0.5f, 0.9f))
                     .build()
     );
 
-    // Register spawn rules and natural spawning
+    // Call this method from ExampleMod to register spawn rules
+    public static void registerSpawns() {
+        BiomeModifications.addSpawn(
+            BiomeSelectors.categories(Biome.Category.PLAINS, Biome.Category.FLOWER_FOREST),
+            SpawnGroup.CREATURE,
+            FAIRY,
+            5, // spawn weight
+            1, // min group
+            2  // max group
+        );
+    }
+}
