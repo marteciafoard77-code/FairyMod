210e86dfb8c8a64d7a5b85f327f7e39e9255ca46
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 13:22:09 -0500

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,20 +5,22 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "modid";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// Define the Fairy Charm item
+	public static final Item FAIRY_CHARM = new FairyCharm();
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Register the Fairy Charm item
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fairy_charm"), FAIRY_CHARM);
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world! Fairy Charm has been registered.");
 	}
 }
