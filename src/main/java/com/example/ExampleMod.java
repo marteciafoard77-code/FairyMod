5a97067f79d70a94394af2d53e8cac6a28c01376
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:40:47 -0500

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -7,11 +7,6 @@ import org.slf4j.LoggerFactory;
 import com.example.item.ModItems; // import your Fairy Charm item
 import com.example.entity.ModEntities; // import your Fairy entity
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.Biome;
-
 public class ExampleMod implements ModInitializer {
     public static final String MOD_ID = "modid";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -23,14 +18,7 @@ public class ExampleMod implements ModInitializer {
         // Register your Fairy Charm item
         ModItems.registerItems();
 
-        // Add Fairy spawn to flower biomes
-        BiomeModifications.addSpawn(
-            BiomeSelectors.categories(Biome.Category.PLAINS, Biome.Category.FLOWER_FOREST),
-            SpawnGroup.CREATURE,
-            ModEntities.FAIRY,
-            5, // weight (spawn chance)
-            1, // min group
-            2  // max group
-        );
+        // Register Fairy entity spawn rules
+        ModEntities.registerSpawns();
     }
 }
