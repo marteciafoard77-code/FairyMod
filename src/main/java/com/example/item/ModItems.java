cbeee5e1a589c182d518fd9dfb3158e21c36f687
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:24:21 -0500

    Update ModItems.java

--- src/main/java/com/example/item/ModItems.java
+++ src/main/java/com/example/item/ModItems.java
@@ -1,1 +1,18 @@
+package com.example.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import com.example.ExampleMod;
+
+public class ModItems {
+
+    public static final Item FAIRY_CHARM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+    public static void registerItems() {
+        Registry.register(Registry.ITEM, new Identifier(ExampleMod.MOD_ID, "fairy_charm"), FAIRY_CHARM);
+    }
+}
 
