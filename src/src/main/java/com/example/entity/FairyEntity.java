90c6159c8934c41ca3194c5f80c3cec79527e271
marteciafoard77-code <marteciafoard77@gmail.com>
2025-10-03 14:20:32 -0500

    Update FairyEntity.java

--- src/src/main/java/com/example/entity/FairyEntity.java
+++ src/src/main/java/com/example/entity/FairyEntity.java
@@ -1,1 +1,49 @@
+package com.example.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.FollowOwnerGoal;
+import net.minecraft.entity.ai.goal.LookAroundGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.SitGoal;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import net.minecraft.particle.ParticleTypes;
+
+public class FairyEntity extends TameableEntity {
+
+    public FairyEntity(EntityType<? extends TameableEntity> type, World world) {
+        super(type, world);
+        this.setTamed(false);
+    }
+
+    @Override
+    protected void initGoals() {
+        this.goalSelector.add(1, new SwimGoal(this));
+        this.goalSelector.add(2, new SitGoal(this));
+        this.goalSelector.add(3, new FollowOwnerGoal(this, 1.0D, 3.0F, 10.0F, false));
+        this.goalSelector.add(4, new LookAroundGoal(this));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        // Spawn sparkles around the fairy
+        if (this.world.isClient && this.isTamed()) {
+            for (int i = 0; i < 2; i++) {
+                double offsetX = (this.random.nextDouble() - 0.5D) * 0.5;
+                double offsetY = this.random.nextDouble() * 0.5 + 0.5;
+                double offsetZ = (this.random.nextDouble() - 0.5D) * 0.5;
+                this.world.addParticle(ParticleTypes.HAPPY_VILLAGER,
+                        this.getX() + offsetX,
+                        this.getY() + offsetY,
+                        this.getZ() + offsetZ,
+                        0, 0, 0);
+            }
+        }
+    }
+
+    // Right-click taming logic will go in a separate handler later
+}
 
